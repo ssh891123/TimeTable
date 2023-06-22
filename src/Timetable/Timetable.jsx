2d4a0fe5f81029e0f83f6d2c1ab44fc2a3a6793a
ssh891123 <ssh891123@gmail.com>
2023-06-22 23:58:32 +0900

    [feat] 강의 시간표 추가 기능
    - Dialog를 팝업하고, Dialog를 통해 강의 시간표에 강의를 추가하는 기능
      구현

--- src/Timetable/Timetable.jsx
+++ src/Timetable/Timetable.jsx
@@ -1,8 +1,9 @@
-import { TableContainer, Typography, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
-import React from 'react';
+import { TableContainer, Typography, Table, TableHead, TableBody, TableRow, TableCell, Button } from '@mui/material';
+import React, { useState, useCallback } from 'react';
 import TimeTableRow from './TimeTableRow';
+import InputModel from '../inputModel/InputModel';
 import { withStyles } from '@mui/styles';
-
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 const hourData = Array.from({length: 11}, (i, j) => j+9);
 const styles = () => ({
@@ -15,13 +16,25 @@ const styles = () => ({
 //[9, 10, ... 19]
 
 function TimeTable({classes}) {
+    const [showModel, setShowModel] = useState(false);
 
+    const handleClose = useCallback(() => {
+        setShowModel(false);
+    }, []);
     return (
         <>
         <TableContainer sx={{width:"80%", minWidth:"650px", marginLeft:"auto", marginRight:"auto", marginTop:"200px"}}>
             <Typography variant="h2" fontWeight={10} component="div" align="center">
                 강의 시간표
             </Typography>
+            <Button
+                variant="contain"
+                sx={{float:"right"}}
+                endIcon={<AddBoxIcon/>}
+                onClick={() => {setShowModel(true)}}
+            >
+                강의 입력
+            </Button>
             <Table className={classes.Table}>
                 <TableHead>
                     <TableRow>
@@ -43,6 +56,7 @@ function TimeTable({classes}) {
                 </TableBody>
             </Table>
         </TableContainer>
+        <InputModel showModel={showModel} handleClose={handleClose}></InputModel>
         </>
     )
 }
---
+++ src/inputModel/InputModel.jsx
@@ -0,0 +1,204 @@
+import { Button, Dialog, DialogActions, DialogContent, 
+    DialogTitle, FormControl, FormControlLabel, FormLabel, 
+    MenuItem, Radio, RadioGroup, Stack, TextField } from "@mui/material";
+import React, { useCallback, useEffect } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { useRecoilState } from 'recoil';
+import { timeTableState } from '../store/store';
+import { v4 as uuidv1 } from 'uuid';
+
+const timeOptions = new Array(12).fill(null).map((e, i) => ({value: i+9, label:i+9})) //e:value, i: index
+//Ex: [{value:9, label:9}], [{value:10, label:10}] ...
+
+const checkOverLap = (A, B) => B.start < A.start ? B.end > A.start : B.start < A.end;
+
+function InputModel({showModel, handleClose}) {
+    const { formState:{errors}, control, getValues, handleSubmit, reset} = useForm();
+
+    const [timeTableData, setTimeTableData] = useRecoilState(timeTableState);
+    
+    //초기값 설정 - 초기값이 없으면 콘솔창에 경고 메시지가 출력
+    useEffect(() => {
+        if(showModel) {
+            reset({
+                lectureName: '',
+                day: 'mon',
+                startTime:9,
+                endTime:10,
+                lectureColor: '#00ffaa',
+            })
+        }
+    }, [showModel]);
+
+    const Submit = useCallback(({lectureName, day, startTime, endTime, lectureColor}) =>{
+        // 중복 검사
+        let valid = true;
+        for(let index=0 ; index<timeTableData[day].length ; index++) {
+            if(checkOverLap(timeTableData[day][index], {start:startTime, end:endTime})) {
+                valid = false;
+                break;
+            }
+        }
+
+        if(!valid) {
+            alert("해당 시간에 강의가 이미 존재합니다.");
+            return;
+        }
+
+        // 강의 시간표 추가
+        const data = {
+            start: startTime,
+            end:endTime,
+            name:lectureName,
+            color:lectureColor,
+            id:uuidv1(),
+        };
+        setTimeTableData((oldTimeData) => ({
+            ...oldTimeData,
+            [day]: [...oldTimeData[day], data]
+        }));
+
+        handleClose();
+    }, [timeTableData, setTimeTableData, handleClose]);
+
+    return (
+        <Dialog open={showModel} onClose={handleClose}>
+            <form onSubmit={handleSubmit((data) => { Submit(data); console.log(data); })}> 
+            <DialogTitle>강의정보 입력</DialogTitle>
+            <DialogContent style={{width:"400px"}}>
+                <Controller //react-hook-form
+                    control={control}
+                    name="lectureName"
+                    rules={{ required:true }}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            error={!!errors.lectureName}
+                            style={{marginTop:"30px", width:"350px"}}
+                            autoComplete="off" //자동완성 기능
+                            label="강의명"
+                        />
+                    )}
+                />
+                {errors.lectureName?.type === 'required' && (
+                    <p style={{color:"#d32f2f"}}>강의명을 입력해주세요.</p>
+                )}
+                <FormControl style={{marginTop:"30px"}}>
+                    <FormLabel>요일</FormLabel>
+                    <Controller
+                        control={control}
+                        name="day"
+                        rules={{required:true}}
+                        render={({field}) =>(
+                            <RadioGroup {...field} style={{display:"block"}}>
+                                <FormControlLabel
+                                value="mon"
+                                control={<Radio />}
+                                label="Mon"
+                                />
+                                <FormControlLabel
+                                value="tue"
+                                control={<Radio />}
+                                label="Tue"
+                                />
+                                <FormControlLabel
+                                value="wed"
+                                control={<Radio />}
+                                label="Wed"
+                                />
+                                <FormControlLabel
+                                value="thu"
+                                control={<Radio />}
+                                label="Thu"
+                                />
+                                <FormControlLabel
+                                value="fri"
+                                control={<Radio />}
+                                label="Fri"
+                                />
+                            </RadioGroup>
+                        )}
+                    />
+                </FormControl>
+                <Stack spacing={3} style={{marginTop:"30px", width:"350px"}}>
+                    <Controller
+                        control={control}
+                        name="startTime"
+                        rules={{ required:true }}
+                        render={({ field }) => (
+                            // error| required가 아닌 경우 error로 인지하기 위함
+                            <TextField
+                                {...field} 
+                                select 
+                                error={!!errors.startTime || !!(errors.endTime?.type === 'validate')}
+                                style={{marginTop:'30px', width:"350px"}}
+                                label="시작 시간"
+                                placeholder="시작 시간 선택"
+                                >
+                                {timeOptions.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        )}
+                    />
+                    {errors.startTime?.type === "required" && (
+                        <p style={{color:"#d32f2f"}}>강의 시작 시간 선택</p>
+                    )}
+
+                    <Controller
+                        control={control}
+                        name="endTime"
+                        rules={{required:true,  validate:(value) => getValues('startTime') < value}}
+                        render={({field}) => (
+                            // error| required가 아닌 경우 error로 인지하기 위함
+                            <TextField 
+                                {...field} 
+                                select 
+                                error={!!errors.endTime}
+                                style={{marginTop:'30px', width:"350px"}}
+                                label="종료 시간"
+                                placeholder="종료 시간 선택"
+                                >
+                                    {timeOptions.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                            </TextField>
+                        )}
+                    />
+                    {errors.endTime?.type === "required" && (
+                        <p style={{color:"#d32f2f"}}>강의 종료 시간 선택</p>
+                    )}
+                    {errors.endTime?.type === "validate" && (
+                        <p style={{color:"#d32f2f"}}>시작시간과 종료시간을 확인해주세요.</p>
+                    )}
+                </Stack>
+                <div style={{marginTop:"30px"}}>
+                    <label htmlFor="lectureColor">시간표 색상:</label>
+                    <Controller
+                        control={control}
+                        name="lectureColor"
+                        render={({field}) => (
+                            <input
+                            {...field}  
+                            style={{marginLeft:"30px"}}
+                            id="lectureColor"
+                            type="color"
+                            />
+                        )}
+                    />
+                </div>
+            </DialogContent>
+            <DialogActions>
+                    <Button onClick={handleClose}>취소</Button>
+                    <Button type="submit">입력</Button>
+            </DialogActions>
+            </form>
+        </Dialog>
+    )
+}
+
+export default InputModel
